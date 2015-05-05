38adb6ad746df08021e3a4183c809d7be57c7dbe
ezefranca <ezequiel@minhavida.com.br>
2015-05-05 11:11:13 -0300

    Initial Commit

---
+++ lastfy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lastfy
+//
+//  Created by Ezequiel Santos on 5/5/15.
+//  Copyright (c) 2015 Ezequiel França. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lastfy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lastfy
+//
+//  Created by Ezequiel Santos on 5/5/15.
+//  Copyright (c) 2015 Ezequiel França. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
